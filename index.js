0e31eaf2627adeb8eefb9caf9b8d1847c8e46eca
unknown <mydulcse62.niter@gmail.com>
2024-05-30 15:49:23 +0600

    maked post get and delete route for favorite services

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// const uri = "mongodb+srv://electronic-web-holder:ZRYtyReZs1jWv7Vj@cluster0.xm07hcd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xm07hcd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,12 +20,11 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
     const database = client.db("RepairRanger");
     const servicesCollection = database.collection("services");
     const bookedServiceCollection = database.collection("bookedService");
     const CommentCollection = database.collection("comment");
+    const favoriteCollection = database.collection("favorite");
     app.get("/", (req, res) => {
       res.send("RepairRanger");
     });
@@ -37,57 +35,68 @@ async function run() {
       res.send(result);
     });
 
-    // for to get length total data
-
     app.get("/service/services-count", async (req, res) => {
       const count = await servicesCollection.estimatedDocumentCount();
       res.send({ count });
     });
 
-    //  for pagination
     app.get("/all-servicefilter", async (req, res) => {
       const size = parseInt(req.query.size);
       const page = parseInt(req.query.page);
-      const cursor = servicesCollection
-        .find()
-        .skip(size * page)
-        .limit(size);
+      const cursor = servicesCollection.find().skip(size * page).limit(size);
       const result = await cursor.toArray();
       res.send(result);
     });
 
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
-      const quary = { _id: new ObjectId(id) };
-      const result = await servicesCollection.findOne(quary);
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
       res.send(result);
     });
-    app.get("/services/:id", async (req, res) => {
-      const id = req.params.id;
-      const quary = { _id: new ObjectId(id) };
-      const result = await  servicesCollection.findOne(quary);
-      res.send(result);
-    });
+
     app.delete("/services/service/delete/:id", async (req, res) => {
       const id = req.params.id;
-      const quary = { _id: new ObjectId(id) };
-      const result = await servicesCollection.deleteOne(quary);
+      const query = { service_id: new ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
       res.send(result);
     });
+
     app.get("/services/service/:email", async (req, res) => {
       const email = req.params.email;
-      const quary = { providerEmail: email };
-      const result = await servicesCollection.find(quary).toArray();
+      const query = { providerEmail: email };
+      const result = await servicesCollection.find(query).toArray();
       res.send(result);
     });
+
     app.post("/services", async (req, res) => {
       const services = req.body;
       const result = await servicesCollection.insertOne(services);
       res.send(result);
     });
+
+    app.post("/favorite", async (req, res) => {
+      const favorite = req.body;
+      const result = await favoriteCollection.insertOne(favorite);
+      res.send(result);
+    });
+
+    app.delete("/favorite/:serviceId", async (req, res) => {
+      const serviceId = req.params.serviceId;
+      const query = { _id: new ObjectId(serviceId) };
+        const result = await favoriteCollection.deleteOne(query);
+       res.send(result)
+    });
+    
+    app.get("/favorites", async (req, res) => {
+      const cursor = favoriteCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.put("/services/update/:id", async (req, res) => {
       const id = req.params.id;
-      const quary = { _id: new ObjectId(id) };
+      const query = { _id: new ObjectId(id) };
       const services = req.body;
 
       update_data = {
@@ -102,55 +111,47 @@ async function run() {
           serviceName: services.serviceName,
         },
       };
-      const result = await servicesCollection.updateOne(quary, update_data);
+      const result = await servicesCollection.updateOne(query, update_data);
       res.send(result);
-      console.log(services);
     });
-    // Send a ping to confirm a successful connection
+
     app.post("/services/service/booked-service", async (req, res) => {
       const services = req.body;
       const result = await bookedServiceCollection.insertOne(services);
       res.send(result);
     });
+
     app.get("/services/service/booked-service/:email", async (req, res) => {
       const email = req.params.email;
-      const quary = { userEmail: email };
-      const result = await bookedServiceCollection.find(quary).toArray();
+      const query = { userEmail: email };
+      const result = await bookedServiceCollection.find(query).toArray();
       res.send(result);
     });
-    app.get(
-      "/services/service/booked-service/too-service/:email",
-      async (req, res) => {
-        const email = req.params.email;
-        const quary = { providerEmail: email };
-        const result = await bookedServiceCollection.find(quary).toArray();
-        res.send(result);
-      }
-    );
 
-    app.patch(
-      "/services/service/booked-service/too-service/update/:id",
-      async (req, res) => {
-        const id = req.params.id;
-        const status = req.body;
-        const quary = { _id: new ObjectId(id) };
-        const updateDoc = {
-          $set: status,
-        };
-        const result = await bookedServiceCollection.updateOne(
-          quary,
-          updateDoc
-        );
+    app.get("/services/service/booked-service/too-service/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { providerEmail: email };
+      const result = await bookedServiceCollection.find(query).toArray();
+      res.send(result);
+    });
 
-        res.send(result);
-      }
-    );
+    app.patch("/services/service/booked-service/too-service/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: status,
+      };
+      const result = await bookedServiceCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
     app.get("/services/service/booked-service/", async (req, res) => {
       const cursor = bookedServiceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-    // comment route
+
     app.post("/comments", async (req, res) => {
       const { text, commentID } = req.body;
       try {
@@ -165,7 +166,6 @@ async function run() {
       }
     });
 
-    // Route to post a reply to a comment
     app.post("/comments/:commentId/replies", async (req, res) => {
       const { commentId } = req.params;
       const { text } = req.body;
@@ -180,7 +180,6 @@ async function run() {
       }
     });
 
-    // Route to get all comments with their replies
     app.get("/comments/:id", async (req, res) => {
       console.log(req.params.id);
       try {
@@ -195,23 +194,11 @@ async function run() {
 
     app.delete("/comments/delete/deleteitem/del/:id", async (req, res) => {
       const id = req.params.id;
-      const quary = { _id: new ObjectId(id) };
-      const result = await CommentCollection.deleteOne(quary);
+      const query = { _id: new ObjectId(id) };
+      const result = await CommentCollection.deleteOne(query);
       res.send(result);
       console.log(id);
     });
-
-    //  serch -------------------------
-    // Server side
-    // app.get('/services/service/booked-service', async (req, res) => {
-    //       const filter = req.query.search;
-    //       const searchResults = await servicesCollection.find({
-    //           serviceName: { $regex: new RegExp(filter, 'i') } // Case-insensitive search
-    //       }).toArray();
-    //       res.send(searchResults);
-    //       console.log(filter);
-
-    // });
 
     app.get("/services/search-datas/search-data/data", async (req, res) => {
       try {
@@ -219,9 +206,9 @@ async function run() {
         const query = {
           serviceName: { $regex: search, $options: "i" },
         };
-        const options = {}; // Options object can be empty if no special options are needed
+        const options = {}; 
 
-        const result = await servicesCollection.find(query, options).toArray(); // Convert the cursor to an array to send as response
+        const result = await servicesCollection.find(query, options).toArray(); 
         res.send(result);
       } catch (error) {
         console.error("Error searching services:", error);
@@ -229,13 +216,9 @@ async function run() {
       }
     });
 
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+    // Uncomment when needed
     // await client.close();
   }
 }
